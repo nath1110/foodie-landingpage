3b7b426b8015c9cc074b84f0e7ddc94d447cbe37
Nathaly <nathynath1110@gmail.com>
2020-10-16 11:57:37 -0600

    Created api archive

---
+++ src/request/api.js
@@ -0,0 +1,79 @@
+
+
+import axios from "axios";
+const API = "https://api.elaniin.dev/";
+
+export const GET_LOCATIONS = async (type) => {
+    try {
+        const result = await axios.get(API+'/api/locations',{
+            params:{
+                type:type
+            }
+        }
+        );
+        return result
+    } catch (error) {
+        console.log("ERROR ", error);
+        return error;
+    }
+}
+
+export const GET_FILTERED_LOCATION = async (type,filter) => {
+    try {
+        const result = await axios.get(API+'/api/locations',{
+                type:type,
+                query:filter
+        });
+        return result
+    } catch (error) {
+        console.log("ERROR ", error);
+        return error;
+    }
+}
+export const GET_CATEGORIES= async ()=>{
+    try{
+        const result =await axios.get(API+'/api/categories');
+        return result;
+    }catch(error){
+        console.log("ERROR ",error);
+        return error;
+    }
+}
+
+export const GET_MENU= async (id)=>{
+    try{
+        const result =await axios.get(API+'/api/menu',{
+                category:id
+        });
+        return result;
+    }catch(error){
+        console.log("ERROR ",error);
+        return error;
+    }
+}
+export const GET_FILTERED_MENU= async (id,filter)=>{
+    try{
+        const result =await axios.get(API+'/api/menu',{
+                category:id,
+                query:filter
+        });
+        return result;
+    }catch(error){
+        console.log("ERROR ",error);
+        return error;
+    }
+}
+
+export const POST_CONTACT= async (name, email, message)=>{
+    try{
+        const result =await axios.get(API+'/api/contact',{
+            "name": name,
+            "email": email,
+            "message": message
+        });
+        return result;
+    }catch(error){
+        console.log("ERROR ",error);
+        return error;
+    }
+}
